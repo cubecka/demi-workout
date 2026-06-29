7ea8da3e9a45115df29e21821885c5cba72fa26c
cubecka <ermk.jozef@gmail.com>
2026-06-29 19:44:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,35 @@
-const CACHE = 'demi-workout-v1';
+const CACHE = "demi-workout-v2";
 
-self.addEventListener('install', e => {
+self.addEventListener("install", (event) => {
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/index.html'])));
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim());
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE) {
+            return caches.delete(key);
+          }
+        })
+      )
+    ).then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+// Network-first strategy (IMPORTANT FIX)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // optionally update cache
+        const clone = response.clone();
+        caches.open(CACHE).then((cache) => {
+          cache.put(event.request, clone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
